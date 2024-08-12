f1c5356eec2fd7a37797d88ac96c6c900a87bafb
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2024-08-12 15:28:10 +0000

    Numbers and Math

---
+++ 01_Basics/06_num_and_math.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5))//it give the decimal
+
+
+// const otherNumber = 238.8966
+
+// console.log(otherNumber.toPrecision(5));//it reture the roundeup value
+
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));//it gives comma for in indian rupees
+
+//===========>Math's<==============
+
+console.log(Math);
+
+console.log(Math.abs(-4));//it gives the absolute value
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(2,6,7,8));
+console.log(Math.max(2,5,6,7
+    ,8));
+console.log(Math.random()*10)
+
+
+
+
+
+
