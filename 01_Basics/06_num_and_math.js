52b4518ba8cd1a23ce5b3dc05cd953d6ba23ddfe
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2024-08-12 15:36:37 +0000

    Some math's function

--- 01_Basics/06_num_and_math.js
+++ 01_Basics/06_num_and_math.js
@@ -22,15 +22,24 @@
 
 console.log(Math);
 
-console.log(Math.abs(-4));//it gives the absolute value
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.1));
-console.log(Math.floor(4.9));
-console.log(Math.min(2,6,7,8));
-console.log(Math.max(2,5,6,7
-    ,8));
-console.log(Math.random()*10)
+// console.log(Math.abs(-4));//it gives the absolute value
+//console.log(Math.random());//it gives the values between 0 to 1
 
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,6,7,8));
+// console.log(Math.max(2,5,6,7,8));
+
+
+
+
+console.log((Math.random()*10)+1)//minimum value is 1
+console.log(Math.floor(Math.random()*10)+1)
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
 
 
 
