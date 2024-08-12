6b1fb5609dbb078153a3f3ffa92f5524c1ca205a
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2024-08-12 07:23:10 +0000

    updated code

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,59 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "JS-hero "
+let str2 ="unbeatable Power"
+console.log(str1+str2);
+
+console.log(1+2);
+console.log( 1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
+
+ console.log(true+2);  // o/p = 3
+ console.log(+true); // o/p = 1
+
+
+ let num1, num2 , num3
+ num1= num2 = num3 = 2+2//not a good practice
+ 
+//   //=======> Precedunce Operator<========
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+// //=========> comparion <=========
+
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2==1)
+console.log(2!=1)
+
+
+console.log(null==0)
+console.log(null > 0)
+console.log(null>=0) // 
+//comparison convert null to a number , treated as  zero that's way null > =0 true and null>0 false
+console.log(undefined == 0)
+console.log(undefined>0)
+console.log(undefined >= 0)
+
+console.log(NaN == 0)
+console.log(NaN > 0)
+console.log(NaN >= 0)
+
+
+//============> Strickly check<=========== 
+console.log("2"===2); //o/p False check the datatype also
---
+++ 01_Basics/dataType_Summary.js
@@ -0,0 +1,81 @@
+//Two types of DataTypes:
+//Primitive : call by value it give the copy of original and changes made in that copy
+
+//7 Types :
+    /*
+    String
+    Number
+    Booleans
+    null
+    undefined
+    symbol
+    BigInt
+     */
+
+//Non-Primitive  : Refernce it directly give the refernce in memory
+    /*
+          Array
+          Objects
+          Function
+    */
+
+//Javascript is a dynamicall typed language not static because vairable hold value of differnt type during runtime
+
+const score = 100
+const scoreValue = 100.3
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol("123")
+const anotherid = Symbol('123')
+console.log(id==anotherid);
+//here we have to note that mention id and anotherid is differnt because Symbol Returns a new unique Symbol value.
+
+const bigNumber = 123435678976545678654567543n
+console.log(typeof bigNumber);
+
+
+//======>Non-Primitive<=======
+
+//Array
+const fruits = ["apple","orange","mango"];
+
+//Object
+let myObj ={
+    name : "doga",
+    age: 22,
+}
+
+//Function
+const myFunction = function(){
+    console.log("OHOOOO");
+
+    
+}
+console.log(typeof fruits);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+
+
+//====================>Memory<====================
+
+//Stack(Primitive)
+//Heap(Non-Prirmitve)
+
+
+let myname = "doga"
+let anothername = myname
+anothername = "doga_the_software Engineer"
+console.log(myname);
+console.log(anothername);
+
+let user ={
+    email : "user@xyzname",
+    upi : "user@ybi"
+}
+
+let userTwo = user
+userTwo.email = "user@xyzname" 
+console.log();
+
+
